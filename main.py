805c1daf06bb955365717ecfa06c48d1dd2eadcc
JulijaZaiceva <124576491+JulijaZaiceva@users.noreply.github.com>
2023-02-12 17:53:15 +0200

    Update main.py
    Checked tabs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# python 3
 from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
@@ -7,23 +8,23 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    result = 0    
+    result = 0
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i))
-            result = result + 1           
-            pass       
+            result = result + 1
+            pass
         if next in ")]}":
             if opening_brackets_stack == 0 and result == 0:
-                return 1            
+                return 1
             elif result > 0 and are_matching(opening_brackets_stack[result - 1].char, next):
-                opening_brackets_stack.pop()             
-                result = result - 1            
+                opening_brackets_stack.pop()
+                result = result - 1
             else:
-                return i + 1       
-        pass    
+                return i + 1
+        pass
     if opening_brackets_stack == 0 or result == 0:
-        return 'Success'    
+        return 'Success'
     else:
         return opening_brackets_stack[-1].position + 1
 
@@ -31,6 +32,7 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     print(mismatch)
+
 
 if __name__ == "__main__":
     main()
