ea6e344547f713349b592e0798e7b20db0016d3d
JulijaZaiceva <124576491+JulijaZaiceva@users.noreply.github.com>
2023-02-15 19:17:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-def find_mismatch(text):
+def find_mismatch(txt):
     opening_brackets_stack = []
     result = 0
-    for i, next in enumerate(text):
+    for i, next in enumerate(txt):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i))
             result = result + 1
@@ -32,7 +32,7 @@ def main():
     txt = input()
     if txt[0] == 'I':
         txt = input()
-        mismatch = find_mismatch(text)
+        mismatch = find_mismatch(txt)
         print(mismatch)
     else:
         print('Incorrect type of input')
